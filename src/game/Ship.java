6a9c3ec68716f0bc9bf1172dfbca1cd1cb855e4c
velixoLaptop <v.lundqvist94@gmail.com>
2015-04-02 23:48:39 +0200

    Added more methods for Ship

--- src/game/Ship.java
+++ src/game/Ship.java
@@ -3,20 +3,30 @@ package game;
 import java.awt.Polygon;
 
 public abstract class Ship {
-	
 	protected Vector2D pos;
-	//protected Vector2D dir;
+	protected double angle;
 	//protected Vector2D vel;
 	
-	public void setDir(float angle){
-		
+	public void setPos(Vector2D pos){
+		this.pos = pos;
 	}
-	public void burn(int amount){
-		//vel.add(dir);
+
+	public void setAngle(double angle){
+		this.angle = angle;
 	}
+
 	public Vector2D getPos(){
 		return pos;
 	}
+
+	public double getAngle(){
+		return angle;
+	}
+	
+	public void burn(int amount){
+		//vel.add(dir);
+	}
+
 	public String toString(){
 		return "[ship]";
 	}
