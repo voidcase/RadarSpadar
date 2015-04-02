c41bcb7618367222a722b896568c65e6ef0c4d0e
velixoLaptop <v.lundqvist94@gmail.com>
2015-04-02 23:45:46 +0200

    Added psuedocode for RenderPanel.paintComponent

--- src/gui/RenderPanel.java
+++ src/gui/RenderPanel.java
@@ -17,5 +17,11 @@ public class RenderPanel extends JPanel {
 	@Override
 	protected void paintComponent(Graphics g) {
 		Graphics2D g2 = (Graphics2D) g;
+		//for each [ship] in Space
+		//	get position, angle and size from ship
+		//	create drawable text image thingy
+		//	rotate entire Graphics2D with ships angle
+		//	draw ship
+		//	rotate Graphics2D back
 	}	
 }
