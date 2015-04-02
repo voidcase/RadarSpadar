79b863e2b9516aa1aa0931c8448ee5f729c147ee
velixoLaptop <v.lundqvist94@gmail.com>
2015-04-02 16:39:19 +0200

    Added timeDelta logic to RadarWindow

--- src/gui/RadarWindow.java
+++ src/gui/RadarWindow.java
@@ -1,7 +1,19 @@
 package gui;
 
+import java.util.Observable;
+import java.util.Observer;
+
 import javax.swing.JFrame;
 
-public class RadarWindow extends JFrame{
+public class RadarWindow extends JFrame implements Observer {
+	private long lastFrameTimeStamp;
+
+	@Override
+	public void update(Observable arg0, Object arg1) {
+		long currentFrameTimeStamp = System.currentTimeMillis();
+		long timeDeltaMillis = currentFrameTimeStamp - lastFrameTimeStamp;
+		lastFrameTimeStamp = currentFrameTimeStamp;
+		//code
+	}
 
 }
