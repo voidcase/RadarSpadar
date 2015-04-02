7d2a05e76a5273a9da131f577e321897ec341270
Erik Grampp <dat13egr@login.student.lth.se>
2015-04-02 17:12:53 +0200

    Added 2DVector

---
+++ src/game/2DVector.java
@@ -0,0 +1,16 @@
+public class 2DVector {
+    private float x, y;
+
+    public 2DVector(float x, float y) {
+	this.x = x;
+	this.y = y;
+    }
+
+    public float getX() {
+	return x;
+    }
+    
+    public float getY() {
+	return y;
+    }
+}
