f7c57bacc2d7582289d0071d6ff5f7d64cbae1b9
Erik Grampp <dat13egr@login.student.lth.se>
2015-04-02 21:36:47 +0200

    Constructor changed, now Space on depends

--- src/gui/RenderPanel.java
+++ src/gui/RenderPanel.java
@@ -4,16 +4,18 @@ import java.awt.Graphics2D;
 import java.awt.Graphics;
 import javax.swing.JPanel;
 import game.Ship;
+import game.Space;
 
 public class RenderPanel extends JPanel {
+	private Space space;
 	
-	public RenderPanel() {
+	public RenderPanel(Space space) {
 		super();
+		this.space = space;
 	}
 	
 	@Override
 	protected void paintComponent(Graphics g) {
 		Graphics2D g2 = (Graphics2D) g;
-	}
-	
+	}	
 }
