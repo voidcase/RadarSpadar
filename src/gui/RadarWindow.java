ea58391c0a2e4af5203409064042d5c00d389119
velixoLaptop <v.lundqvist94@gmail.com>
2015-04-02 17:55:29 +0200

    Added listeneres and stuff for detecting key presses and releases

--- src/gui/RadarWindow.java
+++ src/gui/RadarWindow.java
@@ -1,19 +1,63 @@
 package gui;
 
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.util.Observable;
 import java.util.Observer;
 
+import javax.swing.JComponent;
 import javax.swing.JFrame;
 
-public class RadarWindow extends JFrame implements Observer {
-	private long lastFrameTimeStamp;
+import com.sun.corba.se.spi.orbutil.fsm.Action;
+import com.sun.corba.se.spi.orbutil.fsm.FSM;
+import com.sun.corba.se.spi.orbutil.fsm.Input;
 
+public class RadarWindow extends JFrame implements Observer, KeyListener {
+	private long lastFrameTimeStamp;
+	private boolean arrowUp = false;
+	private boolean arrowDown = false;
+	private boolean arrowRight = false;
+	private boolean arrowLeft = false;
+	
 	@Override
 	public void update(Observable arg0, Object arg1) {
+		update();
+	}
+	
+	private void update() {
 		long currentFrameTimeStamp = System.currentTimeMillis();
 		long timeDeltaMillis = currentFrameTimeStamp - lastFrameTimeStamp;
 		lastFrameTimeStamp = currentFrameTimeStamp;
+		
 		//code
 	}
+	
+	@Override
+	public void keyPressed(KeyEvent e) {
+		if(e.getKeyCode() == KeyEvent.VK_UP)
+			arrowUp = true;
+		if(e.getKeyCode() == KeyEvent.VK_DOWN)
+			arrowDown = true;
+		if(e.getKeyCode() == KeyEvent.VK_RIGHT)
+			arrowRight = true;
+		if(e.getKeyCode() == KeyEvent.VK_LEFT)
+			arrowLeft = true;
+	}
+	
+	@Override
+	public void keyReleased(KeyEvent e) {
+		if(e.getKeyCode() == KeyEvent.VK_UP)
+			arrowUp = false;
+		if(e.getKeyCode() == KeyEvent.VK_DOWN)
+			arrowDown = false;
+		if(e.getKeyCode() == KeyEvent.VK_RIGHT)
+			arrowRight = false;
+		if(e.getKeyCode() == KeyEvent.VK_LEFT)
+			arrowLeft = false;		
+	}
 
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// this is irrelevant
+	}
 }
