b6f7d0d0ab8a37fa55046696d8e8f244d408394b
velixoLaptop <v.lundqvist94@gmail.com>
2015-04-03 00:12:19 +0200

    Added things to/refactored Space

--- src/game/Space.java
+++ src/game/Space.java
@@ -1,27 +1,31 @@
 package game;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Observable;
 
 public class Space extends Observable {
-	
-	ArrayList<Ship> content;
+	List<Ship> ships;
 	
 	public Space(){
-		content = new ArrayList<Ship>();
+		ships = new ArrayList<Ship>();
 	}
 	
 	void spawnShip(Ship s){
-		content.add(s);
+		ships.add(s);
 	}
 
 	public ArrayList<String> scan(Vector2D origin, int range){
 		ArrayList<String> labels = new ArrayList<String>();
-		for(Ship s : content){
+		for(Ship s : ships){
 			if(origin.distance(s.getPos())<=range){
 				labels.add(s.toString());
 			}
 		}
 		return labels;
+	}
+	
+	public List<Ship> getShipList() {
+		return ships;
 	}
 }
