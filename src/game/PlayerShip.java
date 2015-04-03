b070045c7f8ee1a5b68f84d7765f42222d527b54
voidcase <dat13ili@student.lu.se>
2015-04-03 22:26:58 +0200

    made PlayerShip observe KSL

--- src/game/PlayerShip.java
+++ src/game/PlayerShip.java
@@ -1,9 +1,13 @@
 package game;
 
+import java.awt.event.KeyEvent;
+import java.util.Observable;
+import java.util.Observer;
+
 import gui.RadarWindow;
 import datastructures.KeyboardStateListener;
 
-public class PlayerShip extends Ship {
+public class PlayerShip extends Ship implements Observer{
 	
 	KeyboardStateListener keyboard;
 	
@@ -17,5 +21,11 @@ public class PlayerShip extends Ship {
 		// TODO Auto-generated method stub
 		Vector2D arrowDir = keyboard.generateArrowDirection().scale(0.01);
 		vel = vel.add(arrowDir);
+	}
+
+	@Override
+	public void update(Observable arg0, Object arg1) {
+		// TODO Auto-generated method stub
+		KeyEvent e = (KeyEvent)arg1;
 	}
 }
