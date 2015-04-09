d161c205695b365f9381cfc597251e67e4dca334
velixoLaptop <v.lundqvist94@gmail.com>
2015-04-09 09:25:03 +0200

    Pulled sound code out of RadarWindow into its own class, SoundManager

--- src/gui/RadarWindow.java
+++ src/gui/RadarWindow.java
@@ -4,23 +4,17 @@ import game.DrunkenShip;
 import game.GravityWell;
 import game.PlayerShip;
 import game.Ship;
+import game.SoundManager;
 import game.Space;
 import game.Station;
 import game.Vector2D;
 
 import java.awt.event.ComponentEvent;
 import java.awt.event.ComponentListener;
-import java.io.File;
-import java.io.IOException;
-import java.net.URL;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.Map;
 
-import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
-import javax.sound.sampled.LineUnavailableException;
-import javax.sound.sampled.UnsupportedAudioFileException;
 import javax.swing.JFrame;
 
 import datastructures.KeyboardStateListener;
@@ -29,18 +23,13 @@ public class RadarWindow extends JFrame {
 	/** difference of time between frames, in milliseconds*/
 	protected static double timeDelta;
 	private static final long serialVersionUID = -8212981428372798858L;
-	private static final String BACKGROUND_SN = "BACKGROUND";	// BACKGROUND_SOUNDNAME
-	private static final String ALARM_SN = "ALARM";
-	private static final String TARGET_CLOSE_SN = "TARGET_CLOSE";
-	private static final String TARGET_MEDIUM_SN = "TARGET_MEDIUM";
-	private static final String TARGET_FAR_SN = "TARGET_FAR";
 	
 	private KeyboardStateListener keyboardStateListener;
 	private RenderPanel renderPanel;
 	private Space space;
 	private PlayerShip p1;
-	private Map<String, Clip> sounds;
 	private ArrayList<UIField> uiTexts;
+	private SoundManager soundManager;
 	
 	public RadarWindow(Space space) {
 		super("Radar Window");
@@ -64,7 +53,7 @@ public class RadarWindow extends JFrame {
 		setLayout(null);	//ensures that absolute positioning is possible
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		setVisible(true);
-		loadSounds();
+		soundManager = new SoundManager(p1);
 		initLoop();
 	}
 	
@@ -95,75 +84,9 @@ public class RadarWindow extends JFrame {
 	/** Insert game logic here */
 	protected void update() {
 		space.moveAll();
-		manageSounds();
+		soundManager.manageSounds();
 	}
 
-	private void loadSounds() {
-		// TODO implement
-		sounds = new HashMap<String, Clip>();
-		try {
-			Clip background = AudioSystem.getClip();
-			Clip alarm = AudioSystem.getClip();
-			Clip targetClose = AudioSystem.getClip();
-			Clip targetMedium = AudioSystem.getClip();
-			Clip targetFar = AudioSystem.getClip();
-
-			background.open(AudioSystem.getAudioInputStream(new File("res/background_1.wav")));
-			alarm.open(AudioSystem.getAudioInputStream(new File("res/alarm.wav")));
-			targetClose.open(AudioSystem.getAudioInputStream(new File("res/target_close.wav")));
-			targetMedium.open(AudioSystem.getAudioInputStream(new File("res/target_medium.wav")));
-			targetFar.open(AudioSystem.getAudioInputStream(new File("res/target_far.wav")));
-			
-			sounds.put(BACKGROUND_SN, background);
-			sounds.put(ALARM_SN, alarm);
-			sounds.put(TARGET_CLOSE_SN, targetClose);
-			sounds.put(TARGET_MEDIUM_SN, targetMedium);
-			sounds.put(TARGET_FAR_SN, targetFar);
-		} catch (LineUnavailableException | IOException | UnsupportedAudioFileException e) {
-			e.printStackTrace();
-		}
-	}
-	
-	private void manageSounds() {
-		Clip background = sounds.get(BACKGROUND_SN);
-		if(background != null) {
-			background.loop(Clip.LOOP_CONTINUOUSLY);
-		}
-		
-		Clip targetClose = sounds.get(TARGET_CLOSE_SN);
-		Clip targetMedium = sounds.get(TARGET_MEDIUM_SN);
-		Clip targetFar = sounds.get(TARGET_FAR_SN);
-		Ship target = p1.getTarget();
-		if ((target != null) && (targetClose != null) && (targetMedium != null) && (targetFar != null)) {
-			double targetDistance = p1.getPos().distance(target.getPos());
-			if (targetDistance < PlayerShip.RANGE_CLOSE) {
-				targetMedium.stop();
-				targetFar.stop();
-				if(!targetClose.isRunning()) {
-					targetClose.setMicrosecondPosition(0);
-					targetClose.loop(Clip.LOOP_CONTINUOUSLY);
-				}
-			} else if (targetDistance < PlayerShip.RANGE_MEDIUM) {
-				targetClose.stop();
-				targetFar.stop();
-				if(!targetMedium.isRunning()) {
-					targetMedium.setMicrosecondPosition(0);
-					targetMedium.loop(Clip.LOOP_CONTINUOUSLY);
-				}
-			} else if (targetDistance < PlayerShip.RANGE_FAR) {
-				targetClose.stop();
-				targetMedium.stop();
-				if(!targetFar.isRunning()) {
-					targetFar.setMicrosecondPosition(0);
-					targetFar.loop(Clip.LOOP_CONTINUOUSLY);
-				}
-			} else {
-				targetClose.stop();
-				targetMedium.stop();
-				targetFar.stop();
-			}
-		}
-	}
 
 	public static double getTimeDelta(){
 		return timeDelta;
